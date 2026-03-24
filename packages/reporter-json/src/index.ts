0a34c4425c25ff12b77289c661bd6fb339f69055
makotot <makoto.ttn@gmail.com>
2026-03-24 23:33:11 +0900

    remove unused export

--- packages/reporter-json/src/index.ts
+++ packages/reporter-json/src/index.ts
@@ -1,8 +1,6 @@
 import type { Reporter, TreeNode, Out } from '@makotot/canopy-core';
 import { renderJson } from './render-json.js';
 
-export { renderJson } from './render-json.js';
-
 export function createJsonReporter(out: Out): Reporter<TreeNode> {
   return (tree) => out(renderJson(tree));
 }
